d5552c84e11e1f0fbb878d20a7abc41a64215424
qiaojinshuai <q356970306@163.com>
2018-09-15 13:12:42 +0800

    master 增加man

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World,i love u! waman");
+        System.out.println("Hello World,i love u! waman & man");
     }
 }
